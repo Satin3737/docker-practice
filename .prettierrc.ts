89b74e9a2a854eefa6d569ac758b4aaa1da2a1e8
satin <satin3737@gmail.com>
2026-08-18 23:03:59 +0300

    Added prettier and oxlint configs

---
+++ .prettierrc.ts
@@ -0,0 +1,18 @@
+import type {Config} from 'prettier';
+
+const config: Config = {
+    $schema: 'https://json.schemastore.org/prettierrc',
+    semi: true,
+    tabWidth: 4,
+    singleQuote: true,
+    printWidth: 120,
+    trailingComma: 'none',
+    bracketSpacing: false,
+    arrowParens: 'avoid',
+    importOrder: ['^common/load-env', '<THIRD_PARTY_MODULES>', '^@/(.*)$', '^[./(.*)$]'],
+    importOrderSortSpecifiers: true,
+    importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+    plugins: ['@trivago/prettier-plugin-sort-imports']
+};
+
+export default config;
