abb641ca774b1ac45345493015e7dcd05354d5bf
satin <satin3737@gmail.com>
2026-08-19 22:14:33 +0300

    Fixed envs issue with $schema

--- src/plugins/env.ts
+++ src/plugins/env.ts
@@ -5,17 +5,19 @@ import {Environment} from '@/common/const';
 
 const schema = z.object({
     PORT: z.number().default(3000),
+    HOST: z.string().default('0.0.0.0'),
     NODE_ENV: z.enum(Environment).default(Environment.development),
     CORS_ORIGIN: z.string(),
     DATABASE_URL: z.string(),
-    REDIS_URL: z.string(),
+    REDIS_URL: z.string()
 });
 
 export type IEnvConfig = z.infer<typeof schema>;
 
 const envPlugin = fp(
     async fastify => {
-        await fastify.register(env, {schema: z.toJSONSchema(schema)});
+        const {$schema, ...jsonSchema} = z.toJSONSchema(schema);
+        await fastify.register(env, {schema: jsonSchema});
     },
     {name: 'env'}
 );
